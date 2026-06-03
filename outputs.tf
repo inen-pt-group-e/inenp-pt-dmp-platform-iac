8424ebd369bef5eb501f76075f0761efae9a2ae5
andimcce <2510781013@hochschule-burgenland.at>
2026-06-03 14:17:12 +0200

    feat: provision vpc and networking #7 (#17)

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "terraform_state_bucket" {
   description = "GCS bucket used for Terraform remote state"
   value       = "gs://carbon-zone-496411-s6-terraform-state"
 }
+
+output "vpc_name" {
+  description = "Name of the platform VPC"
+  value       = google_compute_network.vpc.name
+}
+
+output "subnet_name" {
+  description = "Name of the platform subnet"
+  value       = google_compute_subnetwork.subnet.name
+}

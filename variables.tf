4d96cc2888f49fbf13bcd3830d64c5e14b17bebf
David B. <102310429+vermout94@users.noreply.github.com>
2026-06-05 13:49:53 +0200

    feat: bootstrap ArgoCD #14 (#24)

--- variables.tf
+++ variables.tf
@@ -15,3 +15,21 @@ variable "zone" {
   type        = string
   default     = "europe-west3-a"
 }
+
+variable "gitops_repo_url" {
+  description = "HTTPS URL of the public GitOps repository watched by ArgoCD (App-of-Apps)"
+  type        = string
+  default     = "https://github.com/inen-pt-group-e/platform-gitops.git"
+}
+
+variable "gitops_target_revision" {
+  description = "Git branch/revision ArgoCD tracks in the GitOps repository"
+  type        = string
+  default     = "main"
+}
+
+variable "argocd_chart_version" {
+  description = "Version of the argo/argo-cd Helm chart to install"
+  type        = string
+  default     = "7.7.11"
+}

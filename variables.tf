baa825b8059a12ebfd9537881d47352684b9a9ef
Andreas Graf <2510781013@hochschule-burgenland.at>
2026-06-14 21:14:36 +0200

    feat: provision GHCR pull-token secret container and writer grant (#41)

--- variables.tf
+++ variables.tf
@@ -21,3 +21,9 @@ variable "argocd_oauth_secret_writers" {
   type        = list(string)
   default     = ["user:r.matthias3@gmail.com"]
 }
+
+variable "ghcr_pull_token_writers" {
+  description = "Principals (user:/group:) allowed to add the GHCR pull token secret version"
+  type        = list(string)
+  default     = ["user:r.matthias3@gmail.com"]
+}
